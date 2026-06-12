56363c01e542bb61dd9de6fbb87b65286c8be5eb
Fahim Muntasir <byefahimmuntasir@gmail.com>
2026-06-13 01:34:06 +0600

    Authentication bug fixed

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../components/ProtectedRoute'
+import { useAuth } from '../contexts/AuthContext'
 import LoginPage from '../pages/LoginPage'
 import SignupPage from '../pages/SignupPage'
 import TransactionsPage from '../pages/TransactionsPage'
@@ -8,10 +9,20 @@ import CategoriesPage from '../pages/CategoriesPage'
 import DashboardPage from '../pages/DashboardPage'
 import ProfilePage from '../pages/ProfilePage'
 
+function RootRedirect() {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return <div className="min-h-screen bg-slate-950 text-slate-100">Loading session...</div>
+  }
+
+  return user ? <Navigate to="/transactions" replace /> : <Navigate to="/login" replace />
+}
+
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<TransactionsPage />} />
+      <Route path="/" element={<RootRedirect />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
 
